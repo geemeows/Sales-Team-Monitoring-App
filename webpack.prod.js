75a896934e555979386e8f3627b8fb55dbd5b338
Mohammed El-Gazouly <m.elgazouly@brimore.com>
2022-06-11 19:46:21 +0200

    stop mangling when minifying bundle

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,19 +1,29 @@
 const { merge } = require('webpack-merge');
-// const commonConfig = require('./webpack.common');
-const devConfig = require('./webpack.dev');
+const commonConfig = require('./webpack.common');
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 const prodConfig = {
-  // mode: 'production',
+  mode: 'production',
   entry: {
     index: './index.js',
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          mangle: false, // Stops renaming variables and params during minification process.
+        },
+      }),
+    ],
   },
   output: {
     filename: '[name].[contenthash].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/dist/',
+    publicPath: '/',
   },
 };
 
-module.exports = merge(devConfig, prodConfig);
+module.exports = merge(commonConfig, prodConfig);
